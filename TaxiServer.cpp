7040526d0828cd776726e0340a2870a1f9873701
ilinetg <galilinetsky@gmail.com>
2016-12-27 18:47:42 +0200

    errors for serverTaxi

--- TaxiServer.cpp
+++ TaxiServer.cpp
@@ -15,35 +15,34 @@
 using namespace std;
 
 int server() {
-    const int server_port
-            = 5678;
+    //socket creator
+    const int server_port = 5678;
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(sock < 0) {
         perror("error creating socket");
     }
+    //bind socket
     struct sockaddr_in sin;
-    memset(&sin, 0, sizeof
-    (sin));
-    sin.sin_family
-            = AF_INET;
-    sin.sin_addr.s_addr
-            = INADDR_ANY;
-    sin.sin_port
-            = htons
-            (server_port);
-    if
-            (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0) {
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(server_port);
+    //bind check
+    if (bind(sock, (struct sockaddr *) &sin, sizeof (sin)) < 0) {
         perror("error binding to socket");
     }
+    //receiving data
     struct sockaddr_in from;
-    unsigned int from_len = sizeof struct sockaddr_in);
+    unsigned int from_len = sizeof(struct sockaddr_in);
     char buffer
     [4096];
     int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
+    // receiving data check
     if(bytes < 0) {
         perror("error reading from socket");
     }
     cout << "The client sent: " << buffer << endl;
+    //sending data check
     int sent_bytes= sendto(sock, buffer, bytes, 0, (struct sockaddr *)&from, sizeof(from));
     if (sent_bytes < 0) {
         perror("error writing to socket");
